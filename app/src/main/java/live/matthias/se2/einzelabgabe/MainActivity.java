6bc35c4896f3c614ce22cfbb09052c90c9dc04d2
Matthias Hülser <matthias@Matthiass-MacBook-Air.local>
2019-03-06 00:13:55 +0100

    Added NetworkHandler but not working yet

--- app/src/main/java/live/matthias/se2/einzelabgabe/MainActivity.java
+++ app/src/main/java/live/matthias/se2/einzelabgabe/MainActivity.java
@@ -1,11 +1,22 @@
 package live.matthias.se2.einzelabgabe;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +35,24 @@ public class MainActivity extends AppCompatActivity {
                 output.setText("Ausgabe: " + stringHandler.handleString(s));
             }
         });
+        Button networkBtn = findViewById(R.id.networkBtn);
+        networkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = findViewById(R.id.inputtext);
+                TextView output = findViewById(R.id.output);
+                String s = input.getText().toString();
+                NetworkHandler networkHandler = new NetworkHandler();
+                try {
+                    output.setText("Ausgabe: " + networkHandler.handleNetwork(s));
+                } catch (IOException e) {
+                    output.setText("Fehler!");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/live/matthias/se2/einzelabgabe/NetworkHandler.java
+++ app/src/main/java/live/matthias/se2/einzelabgabe/NetworkHandler.java
@@ -1,4 +1,19 @@
 package live.matthias.se2.einzelabgabe;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class NetworkHandler {
+    public String handleNetwork(String s) throws IOException {
+        Socket socket = new Socket("se2-isys.aau.at", 53212);
+        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        outToServer.writeBytes(s);
+        String fromServer = inFromServer.readLine();
+        socket.close();
+        return fromServer;
+    }
 }
